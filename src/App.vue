91c4feb2d3cd4cbdf06f2cebf97e548363c9d4f5
1586363574 <1586363574@qq.com>
2023-10-08 21:45:00 +0800

    banner

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+    <tab-bar />
+  </div>
+</template>
+<script setup>
+import tabBar from "@/components/tab_bar/tab-bar.vue";
+</script>
+<style scoped></style>
---
+++ src/components/tab_bar/tab-bar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData" :key="index">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img
+              v-if="currentIndex !== index"
+              :src="getAssetURl(item.image)"
+              alt=""
+            />
+            <img v-else :src="getAssetURl(item.imageActive)" alt="" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+  </div>
+</template>
+<script setup>
+import tabbarData from "@/assets/data/tabbar";
+import { getAssetURl } from "@/utils/get_assets_img";
+import { ref } from "vue";
+const currentIndex = ref(0);
+</script>
+<style lang="less" scoped>
+.tab-bar {
+  img {
+    height: 26px;
+  }
+}
+</style>
